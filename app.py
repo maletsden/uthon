e307d938f26077d211f68f85019f12e46952ac2c
fenix-125 <yurijpasichnyk11@gmail.com>
2019-03-30 10:31:38 +0200

    qr works well

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
 from flask import Flask, render_template, request  # , redirect
 import sqlite3
-
-
-def profile_html_former(user):
-    """
-    Program that returns the HTML with user interface to configure QR
-
-    :param user: str
-    :return: ? (HTML)
-    """
-    return "login.html"
+import qr
 
 
 app = Flask(__name__, static_url_path='/static')
@@ -29,44 +20,47 @@ def register():
     """
     the register page
     """
-    username = request.form.get("login")
-    password = request.form.get("pasw")
-    if not username or not password:
-        return render_template('register_fail.html')
-    with sqlite3.connect('Hizer.db') as data_base:
-        cursor = data_base.cursor()
+    try:
+        username = request.form.get("login")
+        password = request.form.get("pasw")
+        if not username or not password:
+            return render_template('fail.html')
+        with sqlite3.connect('Hizer.db') as data_base:
+            cursor = data_base.cursor()
 
-        check_user = "SELECT ? FROM hizer WHERE ? IN (SELECT username FROM hizer)"
+            check_user = "SELECT ? FROM hizer WHERE ? IN (SELECT username FROM hizer)"
 
-        cursor.execute(check_user, [(username), (username)])
-        result = cursor.fetchall()
+            cursor.execute(check_user, [(username), (username)])
+            result = cursor.fetchall()
 
-    if result != []:
-        return render_template('register_fail.html')
+        if result != []:
+            return render_template('register_fail.html')
 
-    phone = request.form.get("phone")
-    mail = request.form.get("email")
-    telegram = request.form.get("telegram")
-    instagram = request.form.get("instagram")
-    twitter = request.form.get("twitter")
-    facebook = request.form.get("facebook")
+        phone = request.form.get("phone")
+        mail = request.form.get("email")
+        telegram = request.form.get("telegram")
+        instagram = request.form.get("instagram")
+        twitter = request.form.get("twitter")
+        facebook = request.form.get("facebook")
 
-    res = []
-    for media in [username, password, facebook, instagram, telegram, mail,
-                  phone, twitter]:
-        if media == '':
-            media = 'NULL'
-        res.append((media))
+        res = []
+        for media in [username, password, facebook, instagram, telegram, mail,
+                      phone, twitter]:
+            if media == '':
+                media = 'NULL'
+            res.append((media))
 
 
-    with sqlite3.connect('Hizer.db') as data_base:
-        cursor = data_base.cursor()
+        with sqlite3.connect('Hizer.db') as data_base:
+            cursor = data_base.cursor()
 
-        finding_user = """INSERT INTO hizer (username, password, Facebook, Instagram, Telegram,
-                Email, Phone_number, Twitter) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"""
+            finding_user = """INSERT INTO hizer (username, password, Facebook, Instagram, Telegram,
+                    Email, Phone_number, Twitter) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"""
 
-        cursor.execute(finding_user, res)
-        data_base.commit()
+            cursor.execute(finding_user, res)
+            data_base.commit()
+    except Exception:
+        pass
     return render_template("index.html")
 
 
@@ -78,17 +72,17 @@ def sign_up():
     return render_template("register.html")
 
 
-@app.route("/login", methods=["POST"])
-def profile():
-    """
-    What the authorized user see and can manage
-    """
-    if not request.form.get("pasw") or not request.form.get("login"):
-        return render_template("fail.html")
-    login = request.form.get("login")
-    pasw = request.form.get("pasw")
-    # SQL...
-    return render_template(profile_html_former(login))
+# @app.route("/login", methods=["POST"])
+# def profile():
+#     """
+#     What the authorized user see and can manage
+#     """
+#     if not request.form.get("pasw") or not request.form.get("login"):
+#         return render_template("fail.html")
+#     login = request.form.get("login")
+#     pasw = request.form.get("pasw")
+#     # SQL...
+#     return render_template(profile_html_former(login))
 
 
 @app.route("/info", methods=["POST"])
@@ -99,20 +93,28 @@ def friend_post_request():
     """
     return render_template("info.html")
 
-#
-# @app.route('/profile/<command>')
-# def profile():
-#     return render_template("profile.html")
+
+@app.route('/profile', methods=["POST"])
+def profile():
+    login = request.args.get('login', type=str)
+    return render_template("profile.html")
 
 
-@app.route('/profile/<command>/<arg>')
-def profile_sq(command, arg):
+@app.route('/qr', methods=["GET", "POST"])
+def profile_sq():
+    login = request.args.get('login', type=str)
+    arg = request.args.get('arg', type=str)
+    # refresh or add QR image to qr_coders directory
+    qr.render_qr(login, arg)
     # Remove any args that are None
-    return render_template("fail.html")
-
+    return render_template("qr.html",
+                           sorce="/static/qr_codes/qr_{}_{}.jpg"
+                           .format(login, arg))
 
 
 if __name__ == '__main__':
+
+    # create the data base
     # connection = sqlite3.connect('Hizer.db')
     # c = connection.cursor()
     #
