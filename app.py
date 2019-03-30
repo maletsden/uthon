011c0fc28be39f996d1e54b3882fdf90768591dd
maletsden <maletsden@gmail.com>
2019-03-30 13:02:59 +0200

    Resent updates

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request  # , redirect
 import sqlite3
-
 from qr import render_qr
-
+import cv2
+# from camera import VideoCamera, Response
 
 app = Flask(__name__, static_url_path='/static')
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -51,7 +51,6 @@ def register():
                 media = 'NULL'
             res.append((media))
 
-
         with sqlite3.connect('Hizer.db') as data_base:
             cursor = data_base.cursor()
 
@@ -86,13 +85,13 @@ def sign_up():
 #     return render_template(profile_html_former(login))
 
 
-@app.route("/info", methods=["POST"])
-def friend_post_request():
-    """
-    what the friend, who want get the credentials of the specific user through
-    the QR code, will see
-    """
-    return render_template("info.html")
+# @app.route("/info", methods=["POST"])
+# def friend_post_request():
+#     """
+#     what the friend, who want get the credentials of the specific user through
+#     the QR code, will see
+#     """
+#     return render_template("info.html")
 
 
 @app.route('/check_username', methods=["POST"])
@@ -108,10 +107,12 @@ def check_username():
 
     return '1' if result == [] else '0'
 
+
 @app.route('/profile', methods=["POST"])
 def profile():
     login = request.args.get('login', type=str)
     return render_template("share.html")
+
 
 @app.route('/qr', methods=["GET", "POST"])
 def qr():
@@ -123,6 +124,63 @@ def qr():
     return render_template("qr.html",
                            sorce="/static/qr_codes/qr_{}_{}.jpg"
                            .format(login, arg))
+
+
+# def gen(camera):
+#     while True:
+#         frame = camera.get_frame()
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+#
+#
+# @app.route('/scan_qr')
+# def video_feed():
+#     return Response(gen(VideoCamera()),
+#                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+# @app.route('/info', methods=["POST", "GET"])
+# def info():
+#     username = request.args.get("login")
+#     with sqlite3.connect('Hizer.db') as data_base:
+#         cursor = data_base.cursor()
+#
+#         check_user = "SELECT * FROM hizer WHERE username = ?"
+#
+#         cursor.execute(check_user, [(username)])
+#         result = cursor.fetchall()
+#     print(result)
+#
+#     # return '1' if result == [] else '0'
+#     # return render_template("show_contacts.html", )
+#     return '1'
+
+
+@app.route('/info', methods=["POST", "GET"])
+def info():
+    login = request.args.get('login', type=str)
+    arg = request.args.get('arg', type=str)
+    # get info about account
+    with sqlite3.connect('Hizer.db') as data_base:
+        cursor = data_base.cursor()
+
+        check_user = "SELECT * FROM hizer WHERE username = ?"
+
+        cursor.execute(check_user, [(login)])
+        data_base_response = cursor.fetchall()[0]
+
+    db_indexes = [6, 0, 5, 2, 4, 7, 3]
+    lables = ["phone", "username", "email", "facebook", "telegram", "twitter", "instagram"]
+
+    accounts = dict()
+    for i in range(7):
+        accounts[lables[i]] = data_base_response[db_indexes[i]] if arg[i] == "1" else False
+
+    print(accounts)
+
+
+    return render_template("show_contacts.html", accounts=accounts)
+    # accounts=accounts)
 
 
 if __name__ == '__main__':
